663b469cb76c33ba8f6375f4b94e3af91d1ab5c0
Baybars <baybarsstanridagli@gmail.com>
2025-01-12 12:37:39 +0300

    WhichNumberIsBigger

---
+++ WhichNumberIsBigger/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhichNumberIsBigger
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number1, number2;
+            Console.WriteLine("Please Enter The Firs Number : ");
+            number1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please Enter The Second Number : ");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            if (number1 > number2)
+            {
+                Console.WriteLine("First Number is Bigger");
+            }
+            else if (number2 == number1)
+            {
+                Console.WriteLine("The Two Numbers Are Equal");
+            }
+            else
+            {
+                Console.WriteLine("Second Number is Bigger");
+            }
+            Console.ReadKey();
+        }
+    }
+}
